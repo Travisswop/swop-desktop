fbe35031177e9e6bef6eeb70242ca09082e92562
nuruzaman-milon <njmilon1@gmail.com>
2024-04-28 18:22:01 +0600

    link issue solved

--- components/SideBarLink.tsx
+++ components/SideBarLink.tsx
@@ -10,12 +10,21 @@ interface ILinkProps {
 
 const SideBarLink = ({ data, toggle }: ILinkProps) => {
   const pathname = usePathname();
+
+  // Function to determine if the link is active
+  const isActive = () => {
+    if (data.href === "/") {
+      return pathname === "/";
+    }
+    return pathname.startsWith(data.href);
+  };
+
   return (
     <Link href={data.href}>
       <li
-        className={`flex items-center gap-2 ${
-          pathname === data.href && "bg-[#8A2BE21A]"
-        } ${!toggle ? "pl-4" : "px-2"} hover:bg-[#8A2BE21A] py-1.5 rounded-lg`}
+        className={`flex items-center gap-2 ${isActive() && "bg-[#8A2BE21A]"} ${
+          !toggle ? "pl-4" : "px-2"
+        } hover:bg-[#8A2BE21A] py-1.5 rounded-lg`}
       >
         {data.icon} {!toggle && data.title}
       </li>
