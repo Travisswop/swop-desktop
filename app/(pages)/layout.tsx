b1ac2bf39440328c4eea6455bfc981e418fca128
nuruzaman-milon <njmilon1@gmail.com>
2024-05-20 16:51:16 +0600

    in layout check if user is autheticate then can access pages removed

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,18 +2,18 @@ import "../../app/globals.css";
 import LayoutComponent from "@/components/LayoutComponent";
 import { SideBarToggleProvider } from "../../contexts/sideBarToggleContext";
 import TopBar from "@/components/TopBar";
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+// import { auth } from "@/auth";
+// import { redirect } from "next/navigation";
 
 export default async function PageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-  if (!session?.user) {
-    return redirect("/signin");
-  }
+  // const session = await auth();
+  // if (!session?.user) {
+  //   return redirect("/signin");
+  // }
   return (
     <html lang="en">
       <body>
