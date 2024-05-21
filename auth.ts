318bf48052930cc904b60b40eb8fdc26b0fedd03
nuruzaman-milon <njmilon1@gmail.com>
2024-05-21 17:58:44 +0600

    env variable name change for google id and secret

--- auth.ts
+++ auth.ts
@@ -53,8 +53,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           prompt: "consent",
