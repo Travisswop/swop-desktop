e6a8e1beedcfe36e33408bfe790f29bd8667c367
nuruzaman-milon <njmilon1@gmail.com>
2024-06-06 12:20:08 +0600

    api changes for homepage data fetch

--- util/fetchingData/homePageDataFetching.tsx
+++ util/fetchingData/homePageDataFetching.tsx
@@ -1,6 +1,6 @@
 async function getHomePageData(token: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/desktop/user`,
+    `${process.env.NEXT_PUBLIC_API_DEV_URL}/api/v1/desktop/user`,
     {
       headers: {
         authorization: `Bearer ${token}`,
