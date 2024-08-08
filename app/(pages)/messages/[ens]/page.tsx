fb749261e651bdd541f06926c0c8c069d4bf0fdb
nuruzaman-milon <njmilon1@gmail.com>
2024-08-08 13:06:34 +0600

    relocate message component into component folder

--- app/(pages)/messages/[ens]/page.tsx
+++ app/(pages)/messages/[ens]/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { isENSAvailable } from "@/actions/message";
 import isUserAuthenticate from "@/util/isUserAuthenticate";
-import IndividualMessage from "./mainContent";
+import IndividualMessage from "@/components/xmtp/Message";
 
 const IndividualChat = async ({ params }: any) => {
   const session: any = await isUserAuthenticate();
   const response = await isENSAvailable(params.ens, session.accessToken);
-  console.log("response from indivi", response);
+  // console.log("response from indivi", response);
 
   return (
     <div>
