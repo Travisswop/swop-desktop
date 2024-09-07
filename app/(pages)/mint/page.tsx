75669211c4e33161565c765e6a245d29e1f37ecb
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2024-09-07 15:50:24 +0600

    Button links updated

--- app/(pages)/mint/page.tsx
+++ app/(pages)/mint/page.tsx
@@ -39,7 +39,7 @@ const MintDashboard = async () => {
                 />
               ))}
             </div>
-            <Link href={""} className="flex justify-center my-6">
+            <Link href={"/mint/createTemplate"} className="flex justify-center my-6">
               <button className="px-4 py-2 text-sm font-medium border border-gray-400 rounded-lg">
                 Add NFTs To This Collection
               </button>
