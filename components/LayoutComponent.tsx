26a46fd6c30073279ce641b58a111f2f63745d51
nuruzaman-milon <njmilon1@gmail.com>
2024-09-10 13:47:53 +0600

    change sidebar layout

--- components/LayoutComponent.tsx
+++ components/LayoutComponent.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import SideBar from "./SideBar";
 import { Providers } from "@/app/providers";
-import { MotionSection, easeInOutProp } from "@/util/Motion";
+import { MotionSection } from "@/util/Motion";
+// import { MotionSection, easeInOutProp } from "@/util/Motion";
 import useSideBarToggleStore from "@/zustandStore/SideBarToggleStore";
-import useLoggedInUserStore from "@/zustandStore/SetLogedInUserSession";
+// import useLoggedInUserStore from "@/zustandStore/SetLogedInUserSession";
 
 const LayoutComponent = ({ children }: any) => {
   const { toggle, setToggle } = useSideBarToggleStore();
@@ -13,56 +14,71 @@ const LayoutComponent = ({ children }: any) => {
     setToggle();
   };
 
+  // console.log("toggle", toggle);
+
   return (
     <div className={`flex h-screen`}>
       {/* hidden for 2xl 11 */}
-      <MotionSection
-        initial={{ width: toggle ? "6%" : "16%" }} // Initial width when toggle is false
-        animate={{ width: toggle ? "6%" : "16%" }} // Target width based on toggle
-        transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
+      {/* <MotionSection
+        // initial={{ width: toggle ? "6%" : "16%" }} // Initial width when toggle is false
+        // animate={{ width: toggle ? "6%" : "16%" }} // Target width based on toggle
+        // transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
         className={`px-4 block 2xl:hidden overflow-y-auto ${
           !toggle && "w-[30%] xl:w-[25%] 2xl:w-[15%]"
         }`}
       >
         <SideBar toggle={toggle} onToggle={handleToggle} />
-      </MotionSection>
+      </MotionSection> */}
 
       {/* hidden for 2xl 11 */}
-      <MotionSection
-        initial={{ width: toggle ? "94%" : "84%" }} // Initial width when toggle is false
-        animate={{ width: toggle ? "94%" : "84%" }} // Target width based on toggle
-        transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
+      {/* <MotionSection
+        // initial={{ width: toggle ? "94%" : "84%" }} // Initial width when toggle is false
+        // animate={{ width: toggle ? "94%" : "84%" }} // Target width based on toggle
+        // transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
         className={`bg-gray-100 block 2xl:hidden overflow-y-auto ${
           !toggle ? "w-[70%] xl:w-[75%] 2xl:w-[85%]" : "w-[96%]"
         }`}
       >
-        {/* <TopBar /> */}
         <Providers>{children}</Providers>
-      </MotionSection>
+      </MotionSection> */}
 
       {/* only show for 2xl and so on  */}
-      <MotionSection
-        initial={{ width: toggle ? "4%" : "14%" }} // Initial width when toggle is false
-        animate={{ width: toggle ? "4%" : "14%" }} // Target width based on toggle
-        transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
+      {/* <MotionSection
+        // initial={{ width: toggle ? "4%" : "14%" }} // Initial width when toggle is false
+        // animate={{ width: toggle ? "4%" : "14%" }} // Target width based on toggle
+        // transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
         className={`px-4 hidden 2xl:block overflow-y-auto ${
-          !toggle && "w-[30%] xl:w-[25%] 2xl:w-[15%]"
+          !toggle && "w-[30%] xl:w-[25%] 2xl:w-[16%]"
         }`}
       >
         <SideBar toggle={toggle} onToggle={handleToggle} />
-      </MotionSection>
+      </MotionSection> */}
       {/* only show for 2xl and so on  */}
-      <MotionSection
-        initial={{ width: toggle ? "96%" : "86%" }} // Initial width when toggle is false
-        animate={{ width: toggle ? "96%" : "86%" }} // Target width based on toggle
-        transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
+      {/* <MotionSection
+        // initial={{ width: toggle ? "96%" : "86%" }} // Initial width when toggle is false
+        // animate={{ width: toggle ? "96%" : "86%" }} // Target width based on toggle
+        // transition={{ duration: 0.4, ease: easeInOutProp }} // Duration of the animation
         className={`bg-gray-100 hidden 2xl:block overflow-y-auto ${
-          !toggle ? "w-[70%] xl:w-[75%] 2xl:w-[85%]" : "w-[96%]"
+          !toggle ? "w-[70%] xl:w-[75%] 2xl:w-[84%]" : "w-[96%]"
         }`}
+      >
+        <Providers>{children}</Providers>
+      </MotionSection> */}
+
+      {/* for sidebar  */}
+      <section
+        className={`px-4 overflow-y-auto w-[30%] xl:w-[25%] 2xl:w-[16%]`}
+      >
+        <SideBar toggle={toggle} onToggle={handleToggle} />
+      </section>
+
+      {/* for children to right side  */}
+      <section
+        className={`bg-gray-100 overflow-y-auto w-[70%] xl:w-[75%] 2xl:w-[84%]`}
       >
         {/* <TopBar /> */}
         <Providers>{children}</Providers>
-      </MotionSection>
+      </section>
     </div>
   );
 };
