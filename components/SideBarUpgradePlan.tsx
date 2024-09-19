72a80d49e0748abf93be3168d6a1e821e0bd26ed
nuruzaman-milon <njmilon1@gmail.com>
2024-09-19 10:10:05 +0600

    fix hydration issue

--- components/SideBarUpgradePlan.tsx
+++ components/SideBarUpgradePlan.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const SideBarUpgradePlan = ({ toggle, setIsShow }: any) => {
   return (
     <>
-      <motion.div
+      {/* <motion.div
         initial={{
           opacity: toggle ? 0 : 1,
         }}
@@ -39,7 +39,33 @@ const SideBarUpgradePlan = ({ toggle, setIsShow }: any) => {
             Upgrade Plan
           </Link>
         </div>
-      </motion.div>
+      </motion.div> */}
+      <div
+        className={`bg-gray-100 p-4 flex flex-col gap-y-2 2xl:gap-y-4 ${
+          toggle && "hidden"
+        }`}
+      >
+        <AiOutlineShoppingCart size={18} />
+        <div>
+          <p className="mb-2 text-[#454547] font-medium">
+            Unlock Unlimited Access
+          </p>
+          <p className="text-[#454547CC] text-sm 2xl:text-[15px] font-medium">
+            Free NFC with a yearly subscription
+          </p>
+        </div>
+        <div className="flex 2xl:justify-between items-start 2xl:items-center flex-col-reverse 2xl:flex-row 2xl:flex-wrap-reverse">
+          <button
+            onClick={() => setIsShow(false)}
+            className="font-medium text-[#68686A] text-sm 2xl:text-base mt-2 2xl:mt-0"
+          >
+            Dismiss
+          </button>
+          <Link href={"/subscribe"} className="font-semibold text-[#8A2BE2]">
+            Upgrade Plan
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
