5a95e82eac3e964d34eb8d8a50d8f2af875b9b84
nuruzaman-milon <njmilon1@gmail.com>
2024-09-26 14:09:13 +0600

    solve feed time issue

--- util/getFormattedDateWithtime.ts
+++ util/getFormattedDateWithtime.ts
@@ -1,28 +1,10 @@
-export function formatTimestampWithTimeAndDate(timestamp: any) {
-  const date = new Date(timestamp);
-
-  // Extracting date parts
-  const month = date.getMonth() + 1; // Months are 0-based, so we add 1
-  const day = date.getDate();
-  const year = date.getFullYear().toString(); // Get the last two digits of the year
-  let hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, "0"); // Adds leading zero to minutes if necessary
-  const ampm = hours >= 12 ? "PM" : "AM";
-
-  // Converting 24-hour format to 12-hour format
-  hours = hours % 12;
-  hours = hours ? hours : 12; // Convert '0' to '12' for 12 AM/PM
-
-  return `${month}/${day}/${year}  ${hours}:${minutes}${ampm}`;
-}
-
 // export function formatTimestampWithTimeAndDate(timestamp: any) {
-//   const date = new Date(Number(timestamp)); // Ensuring it's a valid number
+//   const date = new Date(timestamp);
 
 //   // Extracting date parts
 //   const month = date.getMonth() + 1; // Months are 0-based, so we add 1
 //   const day = date.getDate();
-//   const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+//   const year = date.getFullYear().toString(); // Get the last two digits of the year
 //   let hours = date.getHours();
 //   const minutes = date.getMinutes().toString().padStart(2, "0"); // Adds leading zero to minutes if necessary
 //   const ampm = hours >= 12 ? "PM" : "AM";
@@ -31,5 +13,25 @@ export function formatTimestampWithTimeAndDate(timestamp: any) {
 //   hours = hours % 12;
 //   hours = hours ? hours : 12; // Convert '0' to '12' for 12 AM/PM
 
-//   return `${month}/${day}/${year} ${hours}:${minutes}${ampm}`;
+//   return `${month}/${day}/${year}  ${hours}:${minutes}${ampm}`;
 // }
+
+export function formatTimestampWithTimeAndDate(
+  timestamp: number | string
+): string {
+  const date = new Date(Number(timestamp)); // Date object interprets the timestamp in local time
+
+  // Extracting date parts
+  const month = date.getMonth() + 1; // Months are 0-based, so we add 1
+  const day = date.getDate();
+  const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, "0"); // Adds leading zero to minutes if necessary
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  // Converting 24-hour format to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Convert '0' to '12' for 12 AM/PM
+
+  return `${month}/${day}/${year} ${hours}:${minutes}${ampm}`;
+}
