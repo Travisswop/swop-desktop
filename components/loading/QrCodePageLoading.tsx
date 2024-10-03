f01f62aa8878e7bda7c17d462fc13610e696daae
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-10-03 13:32:48 +0600

    test update

--- components/loading/QrCodePageLoading.tsx
+++ components/loading/QrCodePageLoading.tsx
@@ -6,15 +6,12 @@ const QrCodePageLoading = () => {
     <Card className="h-screen shadow-none rounded-none p-8 m-6 overflow-hidden">
       <div className="w-full">
         <Skeleton className="rounded-lg h-[40px] w-[200px] mb-4" />
-
         <Skeleton className="h-[70px] w-full border-b" />
         <Skeleton className="h-[70px] w-full border-b" />
         <Skeleton className="h-[70px] w-full border-b" />
         <Skeleton className="h-[70px] w-full border-b" />
         <Skeleton className="h-[70px] w-full border-b" />
-
         <Skeleton className="h-[70px] w-full border-b mb-8" />
-
         <div className="flex items-center justify-center">
           <Skeleton className="rounded-lg h-[50px] w-[200px]" />
         </div>
