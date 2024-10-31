32993dd9be0b4935548568322e8ad55fc970cbaa
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-10-31 17:15:03 +0600

    image and video works perfectly

--- components/feed/PostFeed.tsx
+++ components/feed/PostFeed.tsx
@@ -1,46 +1,26 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import UserImageAvatar from "../Avatar";
-import { FaRegImage } from "react-icons/fa";
 import { MdOutlineDateRange, MdOutlineLocationOn } from "react-icons/md";
 import DynamicPrimaryBtn from "../Button/DynamicPrimaryBtn";
 import Emoji from "./Emoji";
 import GifPickerContent from "./GifPicker";
 import Image from "next/image";
-import PhotoAlbum, { RenderPhotoProps } from "react-photo-album";
 import "react-photo-album/styles.css";
 import ImageContent from "./ImageSelect";
 import { toast } from "react-toastify";
-
-// Render Image Component for Photo Album
-function renderNextImage({ photo, layout, wrapperStyle }: any) {
-  return (
-    <div style={{ ...wrapperStyle, position: "relative" }}>
-      <Image
-        fill
-        src={photo.src}
-        alt={photo.alt || ""}
-        placeholder={photo.blurDataURL ? "blur" : undefined}
-        sizes={layout.width}
-        style={{ objectFit: "cover", borderRadius: "0.5rem" }}
-      />
-    </div>
-  );
-}
+import { AiOutlineClose } from "react-icons/ai"; // Icon for close button
 
 const PostFeed = () => {
   const [postContent, setPostContent] = useState<string>("");
   const [gifContent, setGifContent] = useState<string[]>([]);
-  // const [imageFile, setImageFile] = useState<string[]>([]);
   const [fileError, setFileError] = useState<string>("");
   const [mediaFiles, setMediaFiles] = useState<
     { type: "image" | "video"; src: string }[]
   >([]);
 
   console.log("mediaFiles", mediaFiles);
-  console.log("fileError", fileError);
-
-  const Album: any = PhotoAlbum;
+  console.log("gifContent", gifContent);
 
   // Callback function to handle emoji selection
   const handleEmojiSelect = (emoji: string) => {
@@ -52,17 +32,16 @@ const PostFeed = () => {
     setGifContent((prevContent) => [...prevContent, gif]);
   };
 
-  const photoAlbumData = gifContent.map((gif) => ({
-    src: gif,
-    width: 1200,
-    height: 600,
-  }));
-
   useEffect(() => {
     if (fileError) {
       toast.error(fileError);
     }
   }, [fileError]);
+
+  // Function to remove media item
+  const handleRemoveMedia = (index: number) => {
+    setMediaFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+  };
 
   return (
     <div className="p-6">
@@ -79,184 +58,144 @@ const PostFeed = () => {
             onChange={(e) => setPostContent(e.target.value)}
           ></textarea>
 
-          {gifContent.length > 0 && (
-            <Album
-              layout="rows"
-              photos={photoAlbumData}
-              renderPhoto={renderNextImage}
-              // render={{ image: renderNextImage } as any}
-              targetRowHeight={200}
-            />
+          {/* Render media files */}
+          {mediaFiles.length > 0 && (
+            <div className="mt-4">
+              {mediaFiles.length === 1 && (
+                <div className="relative w-1/2 min-h-96 max-h-[30rem] border border-black bg-black rounded-2xl overflow-hidden">
+                  <button
+                    onClick={() => handleRemoveMedia(0)}
+                    className="absolute top-2 right-2 bg-white p-1 rounded-full hover:bg-gray-300 z-50"
+                  >
+                    <AiOutlineClose size={20} className="text-gray-600" />
+                  </button>
+                  {mediaFiles[0].type === "image" ? (
+                    <Image
+                      src={mediaFiles[0].src}
+                      alt="media"
+                      fill
+                      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                      className="object-contain"
+                    />
+                  ) : (
+                    <video
+                      src={mediaFiles[0].src}
+                      controls
+                      className="w-full h-auto rounded-2xl"
+                      style={{
+                        maxHeight: "30rem",
+                        borderRadius: "0.75rem",
+                      }}
+                    />
+                  )}
+                </div>
+              )}
+
+              {/* Display for 2 media items */}
+              {mediaFiles.length === 2 && (
+                <div className="w-full 2xl:w-[70%] grid grid-cols-2 gap-2 border border-black bg-black rounded-2xl overflow-hidden relative h-auto sm:h-72 md:h-96 xl:h-[28rem]">
+                  {mediaFiles.map((file, index) => (
+                    <div
+                      key={index}
+                      className="relative w-full h-full aspect-[4/3] overflow-hidden"
+                    >
+                      <button
+                        onClick={() => handleRemoveMedia(index)}
+                        className="absolute top-2 right-2 bg-white p-1 rounded-full hover:bg-gray-300 z-50"
+                      >
+                        <AiOutlineClose size={20} className="text-gray-600" />
+                      </button>
+                      {file.type === "image" ? (
+                        <Image
+                          src={file.src}
+                          alt="media"
+                          fill
+                          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                          className="object-cover"
+                        />
+                      ) : (
+                        <video
+                          src={file.src}
+                          controls
+                          className="object-cover w-full h-full"
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+
+              {/* Display for 3 media items */}
+              {mediaFiles.length === 3 && (
+                <div className="w-full bg-black grid grid-cols-3 gap-2 border border-black rounded-2xl overflow-hidden relative h-auto sm:h-72 md:h-96">
+                  {mediaFiles.map((file, index) => (
+                    <div
+                      key={index}
+                      className="relative w-full h-full aspect-[4/3] overflow-hidden"
+                    >
+                      <button
+                        onClick={() => handleRemoveMedia(index)}
+                        className="absolute top-2 right-2 bg-white p-1 rounded-full hover:bg-gray-300 z-50"
+                      >
+                        <AiOutlineClose size={20} className="text-gray-600" />
+                      </button>
+                      {file.type === "image" ? (
+                        <Image
+                          src={file.src}
+                          alt="media"
+                          fill
+                          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                          className="object-cover"
+                        />
+                      ) : (
+                        <video
+                          src={file.src}
+                          controls
+                          className="object-cover w-full h-full"
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+
+              {/* Display for 4 media items */}
+              {mediaFiles.length === 4 && (
+                <div className="w-full 2xl:w-2/3 grid grid-cols-2 gap-1 border-2 border-black bg-black rounded-2xl overflow-hidden relative h-auto sm:h-72 md:h-96 xl:h-[30rem]">
+                  {mediaFiles.map((file, index) => (
+                    <div
+                      key={index}
+                      className="relative w-full h-full aspect-[4/3] overflow-hidden"
+                    >
+                      <button
+                        onClick={() => handleRemoveMedia(index)}
+                        className="absolute top-2 right-2 bg-white p-1 rounded-full hover:bg-gray-300 z-50"
+                      >
+                        <AiOutlineClose size={20} className="text-gray-600" />
+                      </button>
+                      {file.type === "image" ? (
+                        <Image
+                          src={file.src}
+                          alt="media"
+                          fill
+                          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                          className="object-cover"
+                        />
+                      ) : (
+                        <video
+                          src={file.src}
+                          controls
+                          className="object-cover w-full h-full"
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           )}
 
-          {/* {imageFile.length === 1 && (
-            <div className="w-1/2">
-              <Image
-                src={imageFile[0]}
-                alt="image"
-                width={1200}
-                height={900}
-                className="w-full h-auto rounded-2xl"
-              />
-            </div>
-          )} */}
-          {/* {imageFile.length === 2 && (
-            <div className="w-full grid grid-cols-2 items-center gap-5 relative h-auto sm:h-72 md:h-96">
-              {imageFile.map((file, index) => (
-                <div
-                  key={index}
-                  className="relative w-full h-full aspect-[4/3] rounded-2xl overflow-hidden"
-                >
-                  <Image
-                    src={file}
-                    alt="image"
-                    fill
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                    className="object-cover"
-                  />
-                </div>
-              ))}
-            </div>
-          )} */}
-
-          {/* Display media files */}
-          {/* <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {mediaFiles.map((file, index) => (
-              <div
-                key={index}
-                className="relative w-full h-auto rounded-lg overflow-hidden"
-              >
-                {file.type === "image" ? (
-                  <Image
-                    src={file.src}
-                    alt="image"
-                    width={1200}
-                    height={900}
-                    className="w-full h-auto rounded-2xl"
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                  />
-                ) : (
-                  <video
-                    src={file.src}
-                    controls
-                    className="w-full h-auto rounded-2xl"
-                  />
-                )}
-              </div>
-            ))}
-          </div> */}
-          {/* Render based on the number of media files */}
-          <div className="mt-4">
-            {mediaFiles.length === 1 && (
-              <div className="relative w-1/2 border border-black min-h-96 max-h-[30rem] bg-black rounded-2xl overflow-hidden">
-                {mediaFiles[0].type === "image" ? (
-                  <Image
-                    src={mediaFiles[0].src}
-                    alt="media"
-                    fill
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                    className="object-contain"
-                    // style={{
-                    //   objectFit: "cover",
-                    //   borderRadius: "0.75rem", // Smooth rounding for better visual
-                    //   aspectRatio: "4/3", // Maintains a consistent layout across images
-                    // }}
-                  />
-                ) : (
-                  <video
-                    src={mediaFiles[0].src}
-                    controls
-                    className="w-full h-auto rounded-2xl"
-                    style={{
-                      maxHeight: "30rem",
-                      borderRadius: "0.75rem",
-                    }}
-                  />
-                )}
-              </div>
-            )}
-
-            {mediaFiles.length === 2 && (
-              <div className="w-full border border-black 2xl:w-[70%] grid grid-cols-2 bg-black items-center rounded-2xl overflow-hidden gap-2 relative h-auto sm:h-72 md:h-96 xl:h-[28rem]">
-                {mediaFiles.map((file, index) => (
-                  <div
-                    key={index}
-                    className="relative w-full h-full aspect-[4/3] overflow-hidden"
-                  >
-                    {file.type === "image" ? (
-                      <Image
-                        src={file.src}
-                        alt="media"
-                        fill
-                        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                        className="object-cover"
-                      />
-                    ) : (
-                      <video
-                        src={file.src}
-                        controls
-                        className="object-cover w-full h-full"
-                      />
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-            {mediaFiles.length === 3 && (
-              <div className="w-full border border-black bg-black grid grid-cols-3 items-center rounded-2xl overflow-hidden gap-2 relative h-auto sm:h-72 md:h-96">
-                {mediaFiles.map((file, index) => (
-                  <div
-                    key={index}
-                    className="relative w-full h-full aspect-[4/3] overflow-hidden"
-                  >
-                    {file.type === "image" ? (
-                      <Image
-                        src={file.src}
-                        alt="media"
-                        fill
-                        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                        className="object-cover"
-                      />
-                    ) : (
-                      <video
-                        src={file.src}
-                        controls
-                        className="object-cover w-full h-full"
-                      />
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-            {mediaFiles.length === 4 && (
-              <div className="w-full border-2 border-black 2xl:w-2/3 grid grid-cols-2 gap-1 bg-black items-center rounded-2xl overflow-hidden relative h-auto sm:h-72 md:h-96 xl:h-[30rem]">
-                {mediaFiles.map((file, index) => (
-                  <div
-                    key={index}
-                    className="relative w-full h-full aspect-[4/3] overflow-hidden"
-                  >
-                    {file.type === "image" ? (
-                      <Image
-                        src={file.src}
-                        alt="media"
-                        fill
-                        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-                        className="object-cover "
-                      />
-                    ) : (
-                      <video
-                        src={file.src}
-                        controls
-                        className="object-cover w-full h-full"
-                      />
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-
-          <div className="flex items-center gap-10 justify-between mt-4">
+          <div className="flex items-center gap-10 justify-between mt-2">
             <div className="flex items-center gap-3">
               <ImageContent
                 setFileError={setFileError}
